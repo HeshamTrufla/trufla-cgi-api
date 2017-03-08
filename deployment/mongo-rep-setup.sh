8e4f1a507aac5542fe547b2013ef9ee67c7d2a97
Ahmed Ali <ahmedaliabdullah70@gmail.com>
2017-03-08 12:42:41 +0200

    update deployment script

--- deployment/mongo-rep-setup.sh
+++ deployment/mongo-rep-setup.sh
@@ -1,9 +1,11 @@
-
+MONGO_ROOT=DGc3EjoiOfDl6WofAOBvjPw6wQoOZh9S0AXRo0lQgbwVKD8rn5
+MONGO_CLUSTER_USER=3Rd56oRooQw31ips97ASDpo77Gskd5weqSD39713sfjhFF
+MONGO_USER=863cb08a86e8e23721f89aaa63e52ce363391d9a089090884d
 
 echo 'init replication'
 docker exec -it \
     $(docker ps -qf label=com.docker.swarm.service.name=cgistack_mongo-primary) \
-    mongo --eval 'rs.initiate({ _id: "cgi-mongo-rep", members: [{ _id: 0, host: "mongo-primary:27017" }, { _id: 1, host: "mongo-secondary1:27017" }, { _id: 2, host: "mongo-secondary3:27017" }], settings: { getLastErrorDefaults: { w: "majority", wtimeout: 30000 }}})'
+    mongo --eval 'rs.initiate({ _id: "cgi-mongo-rep", members: [{ _id: 0, host: "mongo-primary:27017" }, { _id: 1, host: "mongo-secondary1:27017" }, { _id: 2, host: "mongo-secondary2:27017" }], settings: { getLastErrorDefaults: { w: "majority", wtimeout: 30000 }}})'
 
 docker exec -it $(docker ps -qf label=com.docker.swarm.service.name=cgistack_mongo-primary) mongo --eval 'rs.status()'
 
@@ -15,9 +17,9 @@ docker exec -it $(docker ps -qf label=com.docker.swarm.service.name=cgistack_mon
     mongo admin --eval 'db.createUser({ user: "root", pwd: "$MONGO_ROOT", roles: [ { role: "root", db: "admin" } ] })'
 
 docker exec -it $(docker ps -qf label=com.docker.swarm.service.name=cgistack_mongo-primary) \
-    mongo admin -u root -p $mongo_root --eval 'db.createUser({ user: "cluadmin", pwd: "$MONGO_CLUSTER_USER", roles: [ { role: "clusterAdmin", db: "admin" } ] })'
+    mongo admin -u root -p $MONGO_ROOT --eval 'db.createUser({ user: "cluadmin", pwd: "$MONGO_CLUSTER_USER", roles: [ { role: "clusterAdmin", db: "admin" } ] })'
 
 docker exec -it $(docker ps -qf label=com.docker.swarm.service.name=cgistack_mongo-primary) \
-    mongo admin -u root -p $mongo_root --eval 'db.createUser({ user: "cgiprod", pwd: "$MONGO_USER", roles: [ { role: "readWrite", db: "cgiprod" } ] })'
+    mongo admin -u root -p $MONGO_ROOT --eval 'db.createUser({ user: "cgiprod", pwd: "$MONGO_USER", roles: [ { role: "readWrite", db: "cgiprod" } ] })'
 
 echo 'done'    
