bb647c54434182ee6b50976c0caa2efb03a47449
Ahmed Ali <ahmedaliabdullah70@gmail.com>
2017-03-21 13:20:17 +0200

    revert redis schema back to use primary key

--- app/api/models/AutoPlusRedis.js
+++ app/api/models/AutoPlusRedis.js
@@ -11,6 +11,7 @@ module.exports = {
     },
     autoPlusId: {
       type: 'string',
+      primaryKey: true,
       required: true
     }
   }
--- app/api/models/MVRRedis.js
+++ app/api/models/MVRRedis.js
@@ -11,6 +11,7 @@ module.exports = {
     },
     MVR_ID: {
       type: 'string',
+      primaryKey: true,
       required: true
     }
   }
