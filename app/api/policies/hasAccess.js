c703b9247508b9aef878468e69457fd5736765b7
Wassim Salib <wassim87@gmail.com>
2018-02-23 11:11:06 -0700

    Allow Cors & fix#18
    Fix#18

--- app/api/policies/hasAccess.js
+++ app/api/policies/hasAccess.js
@@ -4,30 +4,26 @@ module.exports = (req, res, next) => {
   // or if this is the last policy, the controller
 
   var method = req.method.toLowerCase() === 'get' ? 'read' : 'write';
-  var apiKey = req.headers['api_key'];
+  var apiKey = req.headers['api_key'] || null;
 
-  if (apiKey) {
 
-    var accessKey = _.find(sails.config.apiKeys, (key) => key.key == apiKey);
+  var accessKey = _.find(sails.config.apiKeys, (key) => key.key == apiKey);
 
-    if (accessKey) {
-      req.apiKey = accessKey;
-      switch (req.options.controller) {
-        case 'mvr':
-          var accessSet = _.find(accessKey.accessPrivillage[method], (access) => access.table == 'mvr');
-          if (accessSet) return next();
-          break;
-        case 'autoplus':
-          var accessSet = _.find(accessKey.accessPrivillage[method], (access) => access.table == 'autoplus');
-          if (accessSet) return next();
-          break;
-      }
+  if (accessKey) {
+    req.apiKey = accessKey;
+    switch (req.options.controller) {
+      case 'mvr':
+        var accessSet = _.find(accessKey.accessPrivillage[method], (access) => access.table == 'mvr');
+        if (accessSet) return next();
+        break;
+      case 'autoplus':
+        var accessSet = _.find(accessKey.accessPrivillage[method], (access) => access.table == 'autoplus');
+        if (accessSet) return next();
+        break;
     }
   }
 
   // User is not allowed
+  return res.forbidden(ResHandlerService.errorObject('INVALID_ACCESS_KEY', true));
 
-  else {
-    return res.forbidden(ResHandlerService.errorObject('INVALID_ACCESS_KEY', true));
-  }
 };
