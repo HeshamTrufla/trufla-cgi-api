d75d486a16f27edb333971e6bc96d1a6090b76a6
Ahmed Ali <contact@ahmedali.me>
2018-10-08 20:50:08 +0200

    rackspace production setup

--- app/config/env/production.js
+++ app/config/env/production.js
@@ -10,22 +10,6 @@
  *
  */
 
-var fs=require('fs');
-
-//This code is a work around to load all the ca chain certificates because NodeJs only see the first certificate in the chain
-var ca=[];
-var chain=fs.readFileSync(__dirname + '/../ssl/fullchain.pem');
-chain= chain.toString().split("\n");
-var cert=[];
-for(line=0; line < chain.length; line++ ){
-	cert.push(chain[line]);
-	if(chain[line].match(/-END CERTIFICATE-/))
-	{
-		ca.push(cert.join("\n"));
-		cert=[];
-	}
-}
-
 module.exports = {
 
   /***************************************************************************
@@ -41,20 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 443,
-
-  // Your SSL certificate and key, if you want to be able to serve HTTP responses
-  // over https:// and/or use websockets over the wss:// protocol
-  // (recommended for HTTP, strongly encouraged for WebSockets)
-  //
-  // In this example, we'll assume you created a folder in your project, `config/ssl`
-  // and dumped your certificate/key files there:
-  ssl: {
-    ca: ca,
-    key: fs.readFileSync(__dirname + '/../ssl/privkey.pem'),
-    cert: fs.readFileSync(__dirname + '/../ssl/cert.pem'),
-  },
-
+  port: 80,
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
    ***************************************************************************/
--- app/config/env/staging.js
+++ app/config/env/staging.js
@@ -10,22 +10,6 @@
  *
  */
 
-var fs=require('fs');
-
-//This code is a work around to load all the ca chain certificates because NodeJs only see the first certificate in the chain
-var ca=[];
-var chain=fs.readFileSync(__dirname + '/../ssl/fullchain.pem');
-chain= chain.toString().split("\n");
-var cert=[];
-for(line=0; line < chain.length; line++ ){
-	cert.push(chain[line]);
-	if(chain[line].match(/-END CERTIFICATE-/))
-	{
-		ca.push(cert.join("\n"));
-		cert=[];
-	}
-}
-
 
 module.exports = {
   
@@ -37,13 +21,7 @@ module.exports = {
     // models: {
     //   connection: 'someMongodbServer'
     // }
-    port: 443,
-
-    ssl: {
-      ca: ca,
-      key: fs.readFileSync(__dirname + '/../ssl/privkey.pem'),
-      cert: fs.readFileSync(__dirname + '/../ssl/cert.pem'),
-    },
+    port: 80,
 
     orm: {_hookTimeout: 120000},
   
